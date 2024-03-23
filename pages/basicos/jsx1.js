32ed0e82a404d6851d18a3cefde727a007429ffb
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-03-23 14:53:09 -0300

    feat: creating firsts pages to test jsx syntax

---
+++ pages/basicos/jsx1.js
@@ -0,0 +1,7 @@
+export default function jsx1Diferente() {
+  return (
+    <div>
+      <h2>JSX #01</h2>
+    </div>
+  );
+}
---
+++ pages/basicos/jsx2.js
@@ -0,0 +1,9 @@
+export default function jsx2() {
+  const conteudo = (
+    <div>
+      <h2>JSX #02</h2>
+    </div>
+  );
+
+  return conteudo;
+}
