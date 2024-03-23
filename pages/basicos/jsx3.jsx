cc8eb0651f3c22f38795f942394a714a0f350fe5
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-03-23 16:34:57 -0300

    feat: returning data in a constant

---
+++ pages/basicos/jsx3.jsx
@@ -0,0 +1,9 @@
+export default function jsx4() {
+  const subtitulo = "Estou no Javascript!";
+  return (
+    <div>
+      <h2>Integração JS e JSX.</h2>
+      <h2>{subtitulo}</h2>
+    </div>
+  );
+}
