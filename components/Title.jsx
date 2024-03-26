069c80f5add0c06b7654a3eb958b49f6fd3eb520
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-03-26 18:02:06 -0300

    feat: exporting and importing components

---
+++ components/Title.jsx
@@ -0,0 +1,8 @@
+export default function Title() {
+  return (
+    <>
+      <h1>Título</h1>
+      <h2>Subtitulo</h2>
+    </>
+  );
+}
---
+++ pages/basicos/usingTitle.jsx
@@ -0,0 +1,9 @@
+import Title from "@/components/Title";
+
+export default function usingTitle() {
+  return (
+    <>
+      <Title />
+    </>
+  );
+}
