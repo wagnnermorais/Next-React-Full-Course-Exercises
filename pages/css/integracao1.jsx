64f5a8911ee598a92f4c593fc32bcc19fa2c89ac
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-03-28 12:16:15 -0300

    style: using normal css file

---
+++ pages/css/integracao1.jsx
@@ -0,0 +1,9 @@
+export default function integracao() {
+  return (
+    <div>
+      <div className="vermelha">Texto 1</div>
+      <div className="azul">Texto 2</div>
+      <div className="branca">Texto 3</div>
+    </div>
+  );
+}
