80271e49cebc5f3a0cda1b66c5c86ef5b8e430db
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-04-03 15:21:39 -0300

    feat: using dynamic classes

--- components/Style.jsx
+++ components/Style.jsx
@@ -4,11 +4,13 @@ export default function Estilo(props) {
       <h2
         style={{
           background: props.numero >= 0 ? "#2d2" : "#d22",
-          color: "#FFF",
+          color: props.color ? "green" : "red",
+          textAlign: props.alignment ? "center" : "left",
         }}
       >
-        Texto
+        Texto #01
       </h2>
+      <h3 className={props.numero >= 0 ? styles.red : "vermelha"}>Texto #02</h3>
     </div>
   );
 }
--- pages/css/usingStyle.jsx
+++ pages/css/usingStyle.jsx
@@ -4,8 +4,8 @@ export default function usingStyle() {
   return (
     <div>
       <h2>Usando estilos!</h2>
-      <Style numero={3} />
-      <Style numero={-5} />
+      <Style numero={3} color={true} alignment={false} />
+      <Style numero={-5} color={false} alignment={true} />
     </div>
   );
 }
