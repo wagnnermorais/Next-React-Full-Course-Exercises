532822616c8011dba212438a324dc76618daf55e
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-04-04 18:38:45 -0300

    feat: creating main page

--- projects/01 - Tabuleiro/pages/index.js
+++ projects/01 - Tabuleiro/pages/index.js
@@ -1,3 +1,13 @@
+import Square from "@/components/Square";
+import styles from "../styles/Home.module.css";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className={styles.container}>
+      <section className={styles.board}>
+        <Square background={false} />
+        <Square background={true} />
+      </section>
+    </main>
+  );
 }
