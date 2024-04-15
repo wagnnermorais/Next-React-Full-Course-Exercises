78be9300713ec5c12acb19f81bf4b5cac1ee8553
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-04-15 17:41:18 -0300

    feat: creating products

---
+++ review/data/listaProdutos.js
@@ -0,0 +1,8 @@
+import Produto from "@/model/produto";
+
+export default [
+  new Produto(1, "iPad", 7500),
+  new Produto(2, "Monitor 24", 1500),
+  new Produto(3, "Computador Gamer", 6500),
+  new Produto(4, "iPhone 24", 4200),
+];
